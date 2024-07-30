ff8ad0af896aed8350a22320f820182805942754
isalimzhanov <isalimzhanov@moysklad.ru>
2024-07-30 08:34:52 +0500

    Добавил таблицу, логотип, кнопку "Add data", footer, authorization modal

--- src/app/app.js
+++ src/app/app.js
@@ -1,6 +1,23 @@
 import html from "./app.html";
 import './app.css'
 
-
 const rootElement = document.getElementById('root');
 rootElement.innerHTML = html;
+
+document.addEventListener('DOMContentLoaded', () => {
+        const addDataButton = document.getElementById('add-data-button');
+        addDataButton.addEventListener('click', () => {
+                const popup = document.getElementById('authentication-modal');
+                const overlay = document.getElementById('overlay');
+                const closePopup = document.getElementById('close-modal');
+                popup.classList.remove('hidden');
+                overlay.classList.remove('hidden');
+                closePopup.addEventListener('click', () => {
+                        popup.classList.add('hidden');
+                        overlay.classList.add('hidden');
+                    }
+                );
+            }
+        );
+    }
+)
